792c7ded961aa9773fa1bacc824e0b4c8b8ff9db
Finley <finley.mwachia12@gmail.com>
2024-06-26 14:18:58 +0300

    add body-parser

--- index1.js
+++ index1.js
@@ -1,13 +1,20 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit",(req,res)=>{
+  console.log(req.body);
 });
 
 app.listen(port, () => {
